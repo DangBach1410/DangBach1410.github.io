fef6e5444f6db3f257d8c494d5079ea24116edb9
trand <trandangbach2004@gmail.com>
2025-01-20 09:15:19 +0700

    First version

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template> 
+  <h1>{{ message }}</h1>
+  <h1>Food</h1>
+  <div id="wrapper">
+    <food-item food-name="Apples"
+    food-desc="Apples are a good source of fiber and vitamin C."
+    :is-favorite="false"
+    />
+    <food-item food-name="Pizza"
+    :is-favorite="true"
+    />
+    <!-- <food-item food-name="Pizza"
+    :is-favorite="true"
+    /> -->
+    <food-item food-name="Rice"
+    :is-favorite="false"
+    />
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }
+</script>
+<style>
+  #app > div:hover {
+    cursor: pointer;
+  }
+  #wrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  #wrapper > div {
+    border: dashed black 1px;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+  img {
+    height: 1.5em;
+    float: right;
+  }
+</style>
---
+++ src/components/FoodItem.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+    <!-- <div v-on:click="increment">   -->
+      <h2>
+        {{ foodName }}
+        <img src="/img_quality.svg" v-show="foodIsFavorite">
+      </h2>
+      <p>{{ foodDesc }}</p>
+      <!-- <p id="red">You have clicked me {{ count }} times</p> -->
+      <button v-on:click="toggleFavorite">Favorite</button>
+    </div>
+</template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        count: 0,
+        foodIsFavorite: this.isFavorite
+      }
+    },
+    props: {
+        foodName: {
+            type: String,
+            required: true
+        },
+        foodDesc: {
+            type: String,
+            required: false,
+            default: 'Description not available',
+            validator: function(value) {
+              if( 20<value.length && value.length<50 ) {
+                return true;
+              }
+              else {
+                return false;
+              }
+            }
+        },
+        isFavorite: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        increment() {
+            this.count++;
+        },
+        toggleFavorite() { 
+          this.foodIsFavorite = !this.foodIsFavorite;
+        }
+        // toggleFavorite() { 
+        //   this.isFavorite = !this.isFavorite;
+        // }
+    }
+  };
+</script>
+  
+<style>
+  /* #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+  #red {
+    font-weight: bold ;
+    color: rgb(144, 12, 12);
+  } */
+</style>
